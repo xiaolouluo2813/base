591ba74fd390a293acde58ffec0762296fa6f30a
xiaolouluo2813 <18576432779@163.com>
2020-07-10 14:24:16 +0800

    first commit

---
+++ src/main/java/com/blockchain/common/base/constant/AppType.java
@@ -0,0 +1,9 @@
+package com.blockchain.common.base.constant;
+
+/***
+ * 服务应用标识
+ */
+public class AppType {
+    public static final String CCT = "CCT";
+    public static final String C2C = "C2C";
+}
